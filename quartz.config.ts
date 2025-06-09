7861fae6d6eb852b2746b728dec5eac83f4c6c52
Ian <ianselbyuk@gmail.com>
2025-06-09 12:34:48 +0100

    Quartz sync: Jun 9, 2025, 12:34 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "ian-eberron",
+    baseUrl: "lemonsprig.github.io/ian-eberron.",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
