6a056293e9f70c476f14e40e8098d829bec2ea95
Ian <ianselbyuk@gmail.com>
2025-06-12 23:37:10 +0100

    component added

---
+++ quartz/components/FolderCardGrid.tsx
@@ -0,0 +1,24 @@
+import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
+
+export const FolderCardGrid: QuartzComponentConstructor = (opts) => {
+  return ({ allContent }: QuartzComponentProps) => {
+    const topFolders = Array.from(
+      new Set(
+        allContent
+          .map((item: QuartzComponentProps["allContent"][number]) => item.slug.split("/")[0])
+          .filter((slug: string) => slug !== "index" && slug !== "")
+      )
+    )
+
+    return (
+    <div class="folder-grid">
+        {topFolders.map((folder: string) => (
+            <a href={`/${folder}/`} class="folder-card">
+                <h2>{folder.replace(/-/g, " ").replace(/\b\w/g, c => c.toUpperCase())}</h2>
+        </a>
+        ))}
+  </div>
+)
+
+  }
+}
