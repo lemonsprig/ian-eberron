dede4dd55248090db481b49a05c24cac5edc5ab6
Ian <ianselbyuk@gmail.com>
2025-06-13 18:26:10 +0100

    Quartz sync: Jun 13, 2025, 6:26 PM

--- quartz/components/FolderCards.tsx
+++ quartz/components/FolderCards.tsx
@@ -5,8 +5,8 @@ import { resolveRelative } from "../util/path"
 interface FolderCardData {
   name: string
   path: string
-  description?: string
-  image?: string
+  description: string
+  image: string
 }
 
 export default (() => {
@@ -34,9 +34,11 @@ export default (() => {
 
     // Define custom mappings for your folders
     const folderImageMap: Record<string, string> = {
-      'eberronlore': '/static/images/EberronLore.png',
-      'houserules': '/static/images/HouseRules.png',
-      'races': '/static/images/Races.png',
+      'eberronlore': 'static/images/EberronLore.png',
+      'houserules': 'static/images/HouseRules.png',
+      'races': 'static/images/Races.png',
+      'races&identity': 'static/images/races.png',
+      'races & identity': 'static/images/races.png',
     }
 
     // Convert to array and create card data
@@ -66,9 +68,12 @@ export default (() => {
               <a href={resolveRelative(fileData.slug!, card.path)} class="folder-card-link">
                 <div class="folder-card-image">
                   <img 
-                    src={resolveRelative(fileData.slug!, card.image!)} 
+                    src={resolveRelative(fileData.slug!, card.image)} 
                     alt={`${card.name} icon`}
-                    onError="this.src='/static/images/default-folder.png'"
+                    onError={(e) => {
+                      const target = e.target as HTMLImageElement;
+                      target.src = resolveRelative(fileData.slug!, 'static/images/default-folder.png');
+                    }}
                   />
                 </div>
                 <div class="folder-card-content">
