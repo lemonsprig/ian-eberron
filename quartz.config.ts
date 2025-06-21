888cdc51800f1c6b59d29771ab7e1b33d550bf6e
Ian <ianselbyuk@gmail.com>
2025-06-21 14:45:57 +0100

    content change

--- quartz.config.ts
+++ quartz.config.ts
@@ -26,24 +26,38 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
+          light: "
+#faf8f8",
+          lightgray: "
+#e5e5e5",
+          gray: "
+#b8b8b8",
+          darkgray: "
+#4e4e4e",
+          dark: "
+#2b2b2b",
+          secondary: "
+#284b63",
+          tertiary: "
+#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
-          gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
+          light: "
+#161618",
+          lightgray: "
+#393639",
+          gray: "
+#646464",
+          darkgray: "
+#d4d4d4",
+          dark: "
+#ebebec",
+          secondary: "
+#7b97aa",
+          tertiary: "
+#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
         },
@@ -89,17 +103,7 @@ const config: QuartzConfig = {
           Component.MobileOnly(Component.Spacer()),
           Component.Search(),
           Component.Darkmode(),
-          Component.DesktopOnly(Component.Explorer({
-            sortFn: (a, b) => {
-              // Sort by weight first, then by title
-              const aWeight = a.frontmatter?.weight ?? 999;
-              const bWeight = b.frontmatter?.weight ?? 999;
-              if (aWeight !== bWeight) {
-                return aWeight - bWeight;
-              }
-              return a.displayName.localeCompare(b.displayName);
-            },
-          })),
+          Component.DesktopOnly(Component.Explorer()),
         ],
         right: [
           Component.Graph(),
@@ -107,17 +111,7 @@ const config: QuartzConfig = {
           Component.Backlinks(),
         ],
       }),
-      Plugin.FolderPage({
-        sortFn: (a, b) => {
-          // Sort by weight first, then by title
-          const aWeight = a.frontmatter?.weight ?? 999;
-          const bWeight = b.frontmatter?.weight ?? 999;
-          if (aWeight !== bWeight) {
-            return aWeight - bWeight;
-          }
-          return a.displayName.localeCompare(b.displayName);
-        },
-      }),
+      Plugin.FolderPage(),
       Plugin.TagPage(),
       Plugin.ContentIndex({
         enableSiteMap: true,
